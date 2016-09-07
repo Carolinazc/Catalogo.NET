df81d1404cb14e035fba307ded36c0fac0ffdb53
carolinazuluaga <carolinazuluaga@MEMOVIL40>
2016-09-07 18:41:03 -0500

    Primer Commit

---
+++ ServicioWeb/CatalogoData/ProductoCategoriaData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogoDto;
+using ;
+
+namespace CatalogoData
+{
+    class ProductoCategoriaData
+    {
+        public void ActualizarProducto()
+        {
+            using (CatalogoEntity)
+                var entity = await _context.Person.Where(c => c.IdPerson == personDto.IdPerson).FirstOrDefaultAsync();
+            if (entity != null)
+            {
+                entity.FirstName = personDto.FirstName;
+                entity.LastName = personDto.LastName;
+                entity.Email = personDto.Email;
+                entity.Movil = personDto.Movil;
+                entity.Telephone = personDto.Telephone;
+                entity.IdCity = personDto.IdCity;
+                if (personDto.ImageProfile != null)
+                {
+                    entity.ImageProfile = personDto.ImageProfile;
+                }
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ServicioWeb/CatalogoDto/CategoriaProductoActualizarDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoDto
+{
+    class CategoriaProductoActualizarDto
+    {
+        public int IdProductoCategoria { get; set; }
+        public int IdProducto { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
