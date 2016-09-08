832329a36d1a857f64b786441def264296a395d7
carolinazuluaga <carolinazuluaga@MEMOVIL40>
2016-09-07 19:12:21 -0500

    Commit 2

--- ServicioWeb/CatalogoData/CategoriasData.cs
+++ ServicioWeb/CatalogoData/CategoriasData.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using CatalogoData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatalogoDto;
 
 namespace CatalogoData
 {
-    class CategoriasData
+    class CategoriasData : IBaseData<CategoriaDto>
     {
+        public CategoriaDto Crear(CategoriaDto entidadDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Editar(CategoriaDto entidadDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminat(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoriaDto> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoriaDto Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ServicioWeb/CatalogoData/IBaseData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogoDto;
+
+namespace CatalogoData
+{
+    //<T> Recibe cualquier tipo clase generica
+    public interface IBaseData<T>
+    {
+        /*Obtener todos
+          Obtener uno por ID
+          Crear 
+          Editar
+          Eliminar*/
+        List<T> Obtener();
+        T Obtener(int id);
+        T Crear(T entidadDto);
+        bool Editar(T entidadDto);
+        bool Eliminat(int id);
+    }
+}
